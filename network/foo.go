84921780735f0b46fabedef0dda06713b179dc66
jacobkristians1@gmail.com <jacobkristians1@gmail.com>
2022-03-04 11:30:44 +0100

    Added WiP network driver

---
+++ network/foo.go
@@ -0,0 +1,34 @@
+package main
+
+import {
+	"Network-go/network/bcast"
+	"Network-go/network/localip"
+	"Network-go/network/peers"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+}
+
+
+
+/* Type: incoming and outgoing (WIP)
+ * Desc: commands nodes to calculate cost for hall call
+ */
+func requestCost()  {
+	
+
+
+}
+
+/* Type: incoming and outgoing
+ * Desc: sends cost calculations from distribution
+ */
+
+func costTransfer()  {
+	
+}
+
+func delegate()  {
+	
+}
