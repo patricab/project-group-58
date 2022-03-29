d81873a1e235a3a1dcdc168ba7e8034411b7d86c
patricab <patric.berthelsen@gmail.com>
2022-03-29 18:15:13 +0200

    fix/feat: solve bug, rework entire FSM
    Solved the bug, but had to rework FSM. FSM is no longer per def FSM.
    Client is now channel-based.
    
    TODO: implement Handler as goroutine

--- fsm/fsm/fsm.go
+++ fsm/fsm/fsm.go
@@ -6,43 +6,64 @@ import (
 	"time"
 )
 
-/* Variables and types*/
-type state int
+/* Global variables */
+var _doors_open bool = false
 
-var _dir elevio.MotorDirection
-var _target elevio.ButtonEvent
-var _obstruction bool
-var _floor int
+func Handler(button chan elevio.ButtonEvent, floor chan int) {
+	/* Local variables */
+	var _floor int
+	var target elevio.ButtonEvent
 
-/* Structs and enums */
-const (
-	IDLE       state = 1
-	MOVING     state = 2
-	DOORS_OPEN state = 3
-)
+	// elevio.Init("localhost:15657", 4)
 
-type current_state struct {
-	idle       chan bool
-	moving     chan bool
-	doors_open chan bool
-	active     state
-}
+	obstr := make(chan bool)
+	stop := make(chan bool)
 
-var _current_state current_state
+	var dir elevio.MotorDirection = elevio.MD_Stop
+	elevio.SetMotorDirection(dir)
 
-/* Public functions */
-/////////////////////
-func Handler(btn <-chan elevio.ButtonEvent, obstruction <-chan bool, stop <-chan bool, floor <-chan int) {
+	go elevio.PollObstructionSwitch(obstr)
+	go elevio.PollStopButton(stop)
+
 	for {
-		/* Set global variables */
-		// _obstruction = <-obstruction
-		fmt.Println("In loop")
-		fmt.Println("Lemme select")
-
 		select {
-		/* Stop button has been pushed */
-		case s := <-stop:
-			fmt.Printf("Stop: %+v\n", s)
+		case target = <-button:
+			fmt.Printf("%+v\n", target)
+			if _floor > target.Floor {
+				dir = elevio.MD_Down
+			} else if _floor < target.Floor {
+				dir = elevio.MD_Up
+			}
+			elevio.SetMotorDirection(dir)
+			elevio.SetButtonLamp(target.Button, target.Floor, true)
+
+		case _floor = <-floor:
+			fmt.Printf("%+v\n", _floor)
+			if _floor > target.Floor {
+				dir = elevio.MD_Down
+				elevio.SetMotorDirection(dir)
+				elevio.SetFloorIndicator(_floor)
+			} else if _floor < target.Floor {
+				dir = elevio.MD_Up
+				elevio.SetMotorDirection(dir)
+				elevio.SetFloorIndicator(_floor)
+			} else if _floor == target.Floor {
+				dir = elevio.MD_Stop
+				elevio.SetButtonLamp(target.Button, target.Floor, false)
+				elevio.SetMotorDirection(dir)
+				elevio.SetFloorIndicator(_floor)
+
+				open_door()
+			}
+
+		case a := <-obstr:
+			fmt.Printf("%+v\n", a)
+			if _doors_open {
+				open_door()
+			}
+
+		case a := <-stop:
+			fmt.Printf("%+v\n", a)
 			/* Clear button lights */
 			for f := 0; f < 4; f++ {
 				for b := elevio.ButtonType(0); b < 3; b++ {
@@ -50,126 +71,16 @@ func Handler(btn <-chan elevio.ButtonEvent, obstruction <-chan bool, stop <-chan
 				}
 			}
 
-			_current_state.idle <- true
-
-		case o := <-obstruction:
-			fmt.Printf("Obstruction: %+v\n", o)
-			set_state(DOORS_OPEN)
-
-		case _floor = <-floor:
-			fmt.Printf("Floor: %+v\n", _floor)
-			elevio.SetFloorIndicator(_floor)
-
-		case _target = <-btn:
-			if _floor > _target.Floor {
-				_dir = elevio.MD_Down
-			} else if _floor < _target.Floor {
-				_dir = elevio.MD_Up
-			}
-			elevio.SetMotorDirection(_dir)
-			elevio.SetButtonLamp(_target.Button, _target.Floor, true)
-
-		default:
-			fmt.Println("Default case")
-			fmt.Printf("Current target %+v\n", _target.Floor)
-
-			if _floor > _target.Floor {
-				set_state(MOVING)
-				_dir = elevio.MD_Down
-				fmt.Println("Going down")
-			} else if _floor < _target.Floor {
-				set_state(MOVING)
-				_dir = elevio.MD_Up
-				fmt.Println("Going up")
-			} else if _floor == _target.Floor {
-				set_state(IDLE)
-				_dir = elevio.MD_Stop
-				fmt.Println("Arrived at target")
-			}
-
-			elevio.SetMotorDirection(_dir)
-
-			/* State machine */
-			fmt.Printf("Current state: %+v\n", _current_state.active)
-			select {
-			case <-_current_state.idle:
-				state_idle()
-			case <-_current_state.moving:
-				state_moving()
-			case <-_current_state.doors_open:
-				state_doors_open()
-			}
+			/* Stop elevator! */
+			elevio.SetMotorDirection(elevio.MD_Stop)
 		}
 	}
 }
 
-/**
-*  @brief Initialize FSM
-*
- */
-func Init() {
-	/* Set initial variable values */
-	// _dir = elevio.MD_Stop
-	_current_state.idle = make(chan bool, 1)
-	_current_state.moving = make(chan bool, 1)
-	_current_state.doors_open = make(chan bool, 1)
-
-	set_state(IDLE)
-}
-
-/* Private functions */
-///////////////////////
-func state_idle() {
-	/* Elevator still, doors closed, waiting for floor */
-	elevio.SetDoorOpenLamp(false)
-	elevio.SetButtonLamp(_target.Button, _target.Floor, false)
-	elevio.SetFloorIndicator(_floor)
-}
-
-func state_moving() {
-	/* Elevator moving in given direction (elevio.MotorDirection) */
-	elevio.SetButtonLamp(_target.Button, _target.Floor, true)
-}
-
-func state_doors_open() {
-	/* Elevator still, doors open */
-	elevio.SetDoorOpenLamp(true)
+func open_door() {
+	_doors_open = true
+	elevio.SetDoorOpenLamp(_doors_open)
 	time.Sleep(3 * time.Second)
-	for {
-		elevio.SetDoorOpenLamp(true)
-		if !_obstruction {
-			set_state(IDLE)
-			break
-		}
-	}
-}
-
-/**
-*  @brief Set current state of FSM
-*
-*  @param state Target state
- */
-func set_state(target state) {
-	/* Check if no state is active */
-	if _current_state.active > 0 {
-		/* Pop current state */
-		switch _current_state.active {
-		case IDLE:
-			<-_current_state.idle
-		case MOVING:
-			<-_current_state.moving
-		case DOORS_OPEN:
-			<-_current_state.doors_open
-		}
-	}
-
-	/* Set new state based on target */
-	switch target {
-	case IDLE:
-		_current_state.idle <- true
-	case MOVING:
-		_current_state.moving <- true
-	case DOORS_OPEN:
-		_current_state.doors_open <- true
-	}
+	_doors_open = false
+	elevio.SetDoorOpenLamp(_doors_open)
 }
--- fsm/main.go
+++ fsm/main.go
@@ -2,34 +2,19 @@ package main
 
 import (
 	"Driver-go/elevio"
-	"fmt"
 	"fsm/fsm"
 )
 
-// var wg = &sync.WaitGroup{}
-
 func main() {
 	elevio.Init("localhost:15657", 4)
-	fsm.Init()
 
 	/* Channel declarations */
-	// btn := make(chan elevio.ButtonEvent, 3)
 	btn := make(chan elevio.ButtonEvent)
-	// obstr := make(chan bool, 3)
-	obstr := make(chan bool)
-	// floor := make(chan int, 3)
 	floor := make(chan int)
-	// stop := make(chan bool, 3)
-	stop := make(chan bool)
 
-	go elevio.PollObstructionSwitch(obstr)
-	go elevio.PollStopButton(stop)
+	/* Channels needed for FSM to run */
 	go elevio.PollButtons(btn)
 	go elevio.PollFloorSensor(floor)
 
-	fmt.Println("Running Handler")
-	// wg.Add(1)
-	fsm.Handler(btn, obstr, stop, floor)
-	// func Handler(btn <-chan elevio.ButtonEvent, obstruction <-chan bool, stop <-chan bool, floor <-chan int)
-	// wg.Wait()
+	fsm.Handler(btn, floor)
 }
