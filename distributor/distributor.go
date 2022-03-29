332eab5c7db0d8f39755edf8f4475f07b21bbd76
napahlm <borgepahlm@gmail.com>
2022-03-29 12:43:21 +0200

    Add descriptions and function structure to cost functions

--- distributor/distributor.go
+++ distributor/distributor.go
@@ -1,5 +1,7 @@
 package distributor
 
+import "strconv"
+
 // const (
 // 	BT_HallUp   ButtonType = 0
 // 	BT_HallDown            = 1
@@ -54,19 +56,75 @@ func watchdog() {
 	// Patric
 }
 
-func calculate_own_cost() {
+func calculate_own_cost(floor, MotorDirection int) int {
 	// Borge
-	// Simple algorithm: Check own floor status + MotorDirection: cost(floor, MD)
-	// More advanced:
+	// Purpose: Calculates own cost for either ...
+	//				1) ... a hall call the local elevator received and want to compare to others
+	//				2) ... another elevator requests this elevator's hall cost
+
+	// Cost algorithm: [cost] = calculate_own_cost(floor, MotorDirection)
+
+	// Distance from desired floor: 				+2*distance [int]
+	//		This includes an elevator going
+	// Motor direction facing way of desired floor: -1*distance [int]
+	// Motor direction facing other way: 			+1*distance [int]
+
+	// Problem scenario:
+	//		One elevator with distance cost 2 but wrong motor direction has the same cost as a
+	//		elevator a whole floor further away but with correct motor direction. If the first elevator is idle,
+	//		it obviously is faster than the other.
+
+	// Anything else to include?
+	// 		- Amount of stops on the way.
+	//		- Distance based on distance to active order + distance from that order floor to desired floor.
+
+	desired_floor := 1 // PLACEHOLDER
+
+	// MAGIC
+
+	return cost
+
 }
 
 func compare_delegate() {
 	// Borge
-	// Input:
-	//	Other elevator costs
-	//	Own cost
-	// Output:
-	//	Delegate to one elevator
+
+	// Cost for all elevators are added to a map
+	// Example of a map after succesfully requesting cost from 3 elevators:
+	//		map = {"cost_0": 1, "cost_1": 0, "cost_2": 2}
+	// ... where "cost_0" always is the local elevator. This works for an arbitrary amount of elevators.
+
+	floor := 1          // PLACEHOLDER
+	motorDirection := 1 // PLACEHOLDER
+	elev_cost := 1      // PLACEHOLDER
+
+	// Create map for comparison
+	compare_map := make(map[string]int)
+
+	// OWN COST
+	local_cost = calculate_own_cost(floor, motorDirection)
+	compare_map["cost_0"] = local_cost
+
+	// GET OTHER N ELEVATOR'S COSTS
+	for i := 1; i < n_elevators+1; i++ {
+		// If not timed out, new cost is added to map
+		for !(timeout) {
+			elev_cost := request_cost(other_elev_port, info_about_hall_order)
+			compare_map["cost_"+strconv.Itoa(i)] = elev_cost
+		}
+
+	}
+
+	// COMPARE COST
+	min_cost := compare_map["cost_0"] // Local elevator is default
+	for elevator, _ := range compare_map {
+		if compare_map[elevator] < min_cost {
+			min_cost = compare_map
+		}
+	}
+
+	// DELEGATE
+	delegate_order(min_cost) // Assuming this variable has enough info about the elevator
 }
 
 // func node() {
