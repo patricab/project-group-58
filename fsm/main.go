d6759aa52d62eb417d1b39785b493bf3d4beea6f
patricab <patric.berthelsen@gmail.com>
2022-03-29 18:24:37 +0200

    MVP

--- fsm/main.go
+++ fsm/main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"Driver-go/elevio"
 	"fsm/fsm"
+	"sync"
 )
 
 func main() {
-	elevio.Init("localhost:15657", 4)
-
-	/* Channel declarations */
+	/* Variables/Channel declarations */
+	var wg sync.WaitGroup
 	btn := make(chan elevio.ButtonEvent)
 	floor := make(chan int)
+
+	/* Initialize elvio driver */
+	elevio.Init("localhost:15657", 4)
 
 	/* Channels needed for FSM to run */
 	go elevio.PollButtons(btn)
 	go elevio.PollFloorSensor(floor)
 
-	fsm.Handler(btn, floor)
+	wg.Add(1) // Add Handler to waitgroup
+	go fsm.Handler(btn, floor)
+	wg.Wait() // Run Handler indefinetly
 }
