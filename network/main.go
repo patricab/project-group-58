ee2301aef79d001323e34bb4959897a218afdb89
patricab <patric.berthelsen@gmail.com>
2022-03-31 13:57:49 +0200

    feat: finish addressing, add main.go

--- network/main.go
+++ network/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"network/network"
+	"strconv"
+	"time"
+)
 
+func main() {
+	var id string
+	var dest string
+	flag.StringVar(&id, "id", "", "id of this peer")
+	flag.StringVar(&dest, "dest", "", "destination id")
+	flag.Parse()
+
+	_id, _ := strconv.Atoi(id)
+	_dest, _ := strconv.Atoi(dest)
+
+	tx := make(chan network.Msg)
+	rx := make(chan network.Msg)
+
+	go network.Handler(id, tx, rx)
+
+	go func() {
+		fmt.Printf("id: %d\n", _id)
+		fmt.Printf("dest: %d\n", _dest)
+
+		msg := network.Msg{_id, _dest, network.CmdDelegate, 1}
+		for {
+			fmt.Println("Sending message")
+			tx <- msg
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	for {
+		select {
+		case m := <-rx:
+			fmt.Printf("Received: %#v\n", m)
+		}
+	}
 }
--- network/network/network.go
+++ network/network/network.go
@@ -1,31 +1,44 @@
 package network
 
-import {
+import (
 	"Network-go/network/bcast"
 	"Network-go/network/localip"
 	"Network-go/network/peers"
-	"flag"
 	"fmt"
-	"os"
-	"time"
+	"strconv"
+)
+
+/* Custom sturcts/types */
+type Cmd int
+
+const (
+	CmdReqCost  Cmd = 0
+	CmdCost         = 1
+	CmdDelegate     = 2
+)
+
+type Msg struct {
+	Id      int
+	Dest    int
+	Command Cmd
+	Data    int // both cost and floor
 }
 
-
-func Handler(Tx chan Msg, Rx chan Msg) {
-	var id string
-	var dest string
-	flag.StringVar(&id, "id", "", "id of this peer")
-	flag.StringVar(&dest, "dest", "", "destination id")
-	flag.Parse()
+func Handler(Id string, Tx chan Msg, Rx chan Msg) {
+	// var dest string
+	// flag.StringVar(&id, "id", "", "id of this peer")
+	// // flag.StringVar(&dest, "dest", "", "destination id")
+	// flag.Parse()
 
 	/* Default address */
-	if id == "" {
+	if Id == "" {
 		localIP, err := localip.LocalIP()
 		if err != nil {
 			fmt.Println(err)
 			localIP = "DISCONNECTED"
 		}
-		id = fmt.Sprintf("%s", localIP)
+		// id = fmt.Sprintf("%s", localIP)
+		Id = localIP
 	}
 
 	// Channel for receiving updates on the id's of the peers that are
@@ -34,26 +47,25 @@ func Handler(Tx chan Msg, Rx chan Msg) {
 	// Disable/enable the transmitter after it has been started.
 	peerTxEnable := make(chan bool)
 
-	_id, _ := strconv.Atoi(id)
-	_dest, _ := strconv.Atoi(dest)
+	_id, _ := strconv.Atoi(Id)
+	// _dest, _ := strconv.Atoi(dest)
 
-	go peers.Transmitter(15648, id, peerTxEnable)
+	go peers.Transmitter(15648, Id, peerTxEnable)
 	go peers.Receiver(15648, peerUpdateCh)
 
 	// We make channels for sending and receiving our custom data types
-	tx := make(chan Msg)
-	rx := make(chan Msg)
+	// _tx := make(chan Msg)
+	_rx := make(chan Msg)
 
-	go bcast.Transmitter(16570, tx)
-	go bcast.Receiver(16570, rx)
+	go bcast.Transmitter(16570, Tx)
+	go bcast.Receiver(16570, _rx)
 
-	for{
+	for {
 		select {
-		case m <- rx:
+		case m := <-_rx:
 			if m.Dest == _id {
 				Rx <- m
-			}	
+			}
 		}
 	}
 }
-
