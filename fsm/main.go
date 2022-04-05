95c6d34199721062141e74cd450660bfb9055d9c
patricab <patric.berthelsen@gmail.com>
2022-04-05 14:28:24 +0200

    fix: update main.go for recent FSM changes

--- fsm/main.go
+++ fsm/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Driver-go/elevio"
+	"fmt"
 	"fsm/fsm"
 	"sync"
 )
@@ -12,6 +13,7 @@ func main() {
 	var wg sync.WaitGroup
 	btn := make(chan elevio.ButtonEvent)
 	floor := make(chan int)
+	finished := make(chan bool)
 
 	/* Initialize elvio driver */
 	elevio.Init("localhost:15657", 4)
@@ -20,7 +22,16 @@ func main() {
 	go elevio.PollButtons(btn)
 	go elevio.PollFloorSensor(floor)
 
+	go func() {
+		for {
+			select {
+			case <-finished:
+				fmt.Println("Reached floor")
+			}
+		}
+	}()
+
 	wg.Add(1) // Add Handler to waitgroup
-	go fsm.Handler(btn, floor, _current_state)
+	go fsm.Handler(btn, floor, _current_state, finished)
 	wg.Wait() // Run Handler indefinetly
 }
