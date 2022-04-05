56038853788b27911fe0cb96670ed8df8812a2c0
napahlm <borgepahlm@gmail.com>
2022-04-05 16:25:44 +0200

    Add delegation of hall orders

--- distributor/distributor/distributor.go
+++ distributor/distributor/distributor.go
@@ -2,18 +2,28 @@ package distributor
 
 import (
 	"Driver-go/elevio"
+	"Network-go/network/peers"
 	"fmt"
 	"fsm/fsm"
 	"network/network"
 	. "network/network"
+	"time"
 )
+
+type Msg struct {
+	Id      int
+	Dest    int
+	Command Cmd
+	Data    int // both cost and floor
+}
 
 /* Global variables */
 // var id int
 
 // var floor int
 // var costArr []int
-var priorityQueue []elevio.ButtonEvent
+var priorityQueue []int //[]elevio.ButtonEvent
+var costArray []Msg
 
 // var costTimeout = 500
 
@@ -21,13 +31,15 @@ var priorityQueue []elevio.ButtonEvent
 var tx = make(chan network.Msg)
 var rx = make(chan network.Msg)
 
-// var _peers = make(chan peers.PeerUpdate)
+var _peers = make(chan peers.PeerUpdate)
 var _btn = make(chan elevio.ButtonEvent)
 var btn = make(chan elevio.ButtonEvent)
 
 // var finished = make(chan bool)
 var _floor = make(chan int)
 var current_state fsm.State
+
+const N_ELEVATORS = 3
 
 func Distributor(_id int) {
 	/* Variables */
@@ -49,9 +61,9 @@ func Distributor(_id int) {
 		select {
 		case b := <-btn:
 			if b.Button == 2 { // Cab
-				add_to_queue(b)
+				add_to_queue(b.Floor)
 			} else { // Hall
-				compare_delegate(b)
+				delegate_hall(b)
 			}
 		case m := <-rx:
 			if m.Command == CmdDelegate {
@@ -62,8 +74,7 @@ func Distributor(_id int) {
 				m.Data = cost
 				tx <- m
 			} else if m.Command == CmdCost {
-				fmt.Println("Append cost to cost array")
-				// Add cost to cost array (append(costArr, m.Data))
+				costArray = append(costArray, m)
 			}
 		}
 	}
@@ -76,67 +87,52 @@ func Distributor(_id int) {
 // 	tx <- msg
 // }
 
-// func request_cost(target int, costReady chan bool) {
-// 	// Check num of connected nodes
-// 	numNodes := len(_peers.Peers) - 1
+func request_cost(target int) {
 
-// 	// Send cost req
-// 	tx <- Msg{id, 0, CmdReqCost, target}
-// 	timer := time.NewTimer(time.Duration(costTimeout) * time.Millisecond)
+	// Check num of connected nodes
+	numNodes := len(_peers.Peers) - 1
+	//timer := time.NewTimer(time.Duration(costTimeout) * time.Millisecond)
+	timeout := time.After(500 * time.Millisecond)
 
-// 	for {
-// 		select {
-// 		case <-timer.C:
-// 			break
-// 		default:
-// 			if len(costArr) == numNodes {
-// 				break
-// 			}
-// 		}
-// 	}
-// }
+	// Send cost req
+	tx <- Msg{id, 0, CmdReqCost, target}
+
+	for {
+		select {
+		case <-timeout:
+			return
+		default:
+			if len(costArray) == numNodes {
+				break
+			}
+		}
+	}
+}
 
 // func watchdog() {
 // 	// Patric
 // }
 
-// func calculate_own_cost() (cost int) {
-// 	// Borge
+func calculate_own_cost(dest_floor int) (cost int) {
 
-// 	// Switch case focuses on calculating cost intuitively based on the time it would take in seconds
-// 	// Constants used
-// 	//		Floor travel time (2-3 seconds?)
-// 	//		Moving penalty (1)
-// 	// Used variables
-// 	//		Current floor
-// 	//		Destination floor
+	FLOOR_TRAVEL_TIME := 2
+	MOVING_PENALTY := 1
+	DOOR_OPEN_TIME := 3
 
-// 	FLOOR_TRAVEL_TIME := 2 // sec
-// 	MOVING_PENALTY := 1    // To distinguish moving elevators from idle ones, so as to not inconvenience anyone waiting for an elevator
-// 	DOOR_OPEN_TIME := 3    // sec
+	switch current_state {
+	case fsm.DOORS_OPEN:
+		cost = abs(curr_floor-dest_floor)*FLOOR_TRAVEL_TIME + DOOR_OPEN_TIME/2
+		return cost
+	case fsm.IDLE:
+		cost = abs(curr_floor-dest_floor) * FLOOR_TRAVEL_TIME
+		return cost
+	case fsm.MOVING:
+		cost = abs(curr_floor-dest_floor)*FLOOR_TRAVEL_TIME*FLOOR_TRAVEL_TIME + MOVING_PENALTY
+		return cost
+	}
+}
 
-// 	switch current_state {
-
-// 	// case FailureMode:
-// 	// 	cost = 999
-
-// 	case fsm.DOORS_OPEN:
-// 		// No obstruction
-// 		cost = abs(curr_floor-dest_floor)*FLOOR_TRAVEL_TIME + DOOR_OPEN_TIME/2
-
-// 	case fsm.IDLE:
-// 		cost = abs(curr_floor-dest_floor) * FLOOR_TRAVEL_TIME
-
-// 	case fsm.MOVING: // If its moving, it has an order
-// 		// cost is distance from current floor
-// 		cost = abs(curr_floor-dest_floor)*FLOOR_TRAVEL_TIME*FLOOR_TRAVEL_TIME + MOVING_PENALTY
-// 	}
-
-// 	return cost
-
-// }
-
-func add_to_queue(new_item elevio.ButtonEvent) {
+func add_to_queue(new_item int) {
 
 	fmt.Println("Appending to queue")
 	priorityQueue = append(priorityQueue, new_item)
@@ -146,69 +142,41 @@ func add_to_queue(new_item elevio.ButtonEvent) {
 	priorityQueue = priorityQueue[1:]
 }
 
-func compare_delegate(new_item elevio.ButtonEvent) {
+func delegate_hall(new_item elevio.ButtonEvent) {
 
-	if false {
-		// Add hall call to FSM?
-	} else {
-		fmt.Println("Appending to queue")
-		priorityQueue = append(priorityQueue, new_item)
+	// Msg
+	local_msg := Msg{0, 0, CmdCost, 0}
 
-		fmt.Println("Executing order")
-		_btn <- priorityQueue[0]
-		priorityQueue = priorityQueue[1:]
+	// Calculate own cost
+	dest_floor := new_item.Floor
+	local_msg.Data = calculate_own_cost(dest_floor)
+
+	// Request other elevator's cost
+	costArray = nil // Clear all elements
+	costArray = append(costArray, local_msg)
+	request_cost(dest_floor)
+
+	// Delegate to lowest cost (Default: local)
+	min_cost := local_msg.Data
+	delegate_id := 99
+
+	for _, message := range costArray {
+		if message.Data < min_cost {
+			min_cost = message.Data
+			delegate_id = message.Id
+		}
 	}
-	// Compare replied cost + own cost, which is the lowest?
-	// request_cost()
 
-	// Delegate order/take order itself?
-	// msg = Msg{id, dest, CmdDelegate, floor}
-	// tx <- msg
+	dest := 10
 
-	// Append order to queue (or re-evaluate queue priority?)
-	// fmt.Println("Appending to queue")
-	// priorityQueue = append(priorityQueue, new_item)
+	msg := Msg{
+		delegate_id,
+		dest,
+		CmdDelegate,
+		dest_floor,
+	}
 
-	// // Execute order (send to FSM)
-	// fmt.Println("Executing order")
-	// btn <- priorityQueue[0]
-
-	// Cost for all elevators are added to a map
-	// Example of a map after succesfully requesting cost from 3 elevators:
-	//		map = {"cost_0": 1, "cost_1": 0, "cost_2": 2}
-	// ... where "cost_0" always is the local elevator. This works for an arbitrary amount of elevators.
-
-	// floor := 1          // PLACEHOLDER
-	// motorDirection := 1 // PLACEHOLDER
-	// elev_cost := 1      // PLACEHOLDER
-
-	// // Create map for comparison
-	// compare_map := make(map[string]int)
-
-	// // OWN COST
-	// local_cost = calculate_own_cost(floor, motorDirection)
-	// compare_map["cost_0"] = local_cost
-
-	// // GET OTHER N ELEVATOR'S COSTS
-	// for i := 1; i < n_elevators+1; i++ {
-	// 	// If not timed out, new cost is added to map
-	// 	for !(timeout) {
-	// 		elev_cost := request_cost(other_elev_port, info_about_hall_order)
-	// 		compare_map["cost_"+strconv.Itoa(i)] = elev_cost
-	// 	}
-
-	// }
-
-	// // COMPARE COST
-	// min_cost := compare_map["cost_0"] // Local elevator is default
-	// for elevator, _ := range compare_map {
-	// 	if compare_map[elevator] < min_cost {
-	// 		min_cost = compare_map
-	// 	}
-	// }
-
-	// DELEGATE ORDER
-	// delegate_order(min_cost) // Assuming this variable has enough info about the elevator
+	tx <- msg
 }
 
 // func node() {
